77a2a803bd9a1ff3d9b59d4ec5aaccde406f6be4
Toufiqul Islam <mdtoufiq2342@gmail.com>
2021-01-07 16:21:03 +0600

    Sum of Round Numbers- Sub 1
    Accepted

--- Codeforces/Sum of Round Numbers-1352A/1352A.cpp
+++ Codeforces/Sum of Round Numbers-1352A/1352A.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <stack>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int T;
+    cin >> T;
+    while(T--){
+        int i = 1, num,j=10,temp;
+        stack <int > s;
+        cin >> num;
+        while(num){
+            temp = num % j;
+            num = num / j;
+            if(temp) {
+                s.push(temp * i);
+            }
+            i = i*j;
+        }
+        cout << s.size() <<endl;
+        while(!s.empty()){
+            cout << s.top() << " ";
+            s.pop();
+        }
+        cout << endl;
+    }
     return 0;
 }
